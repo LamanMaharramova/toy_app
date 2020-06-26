73adc57fdf70307fe00b187204746a72b9dd7998
Laman-Maharramova <meherremova55@gmail.com>
2020-06-26 19:43:38 +0200

    Modified set_user method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -64,7 +64,10 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find_by(params[:id])
+      if User.exists?(params[:id])
+        @user = User.find(params[:id])
+      end
+      #@user = User.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
